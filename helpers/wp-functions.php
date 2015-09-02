ee95472d07783965b8d770d3663d44f5dd5638d3
Moe Loubani <moe@loubani.com>
2015-09-02 17:24:02 -0400

    removed unused code

--- helpers/wp-functions.php
+++ helpers/wp-functions.php
@@ -11,26 +11,3 @@ add_action('wp_enqueue_scripts', 'wptrebs_styles_scripts');
 // Image sizes for feed and gallery/carousel
 add_image_size('wptrebs_feed_img', 169, 115, true);
 add_image_size('wptrebs_gallery_img', 125, 75, true);
-
-//Wtf is this shit?
-//On plugin activation schedule our daily database backup
-//register_activation_hook( __FILE__, 'wi_create_daily_property_schedule' );
-/*function wi_create_daily_property_schedule(){
-	//Use wp_next_scheduled to check if the event is already scheduled
-	$timestamp = wp_next_scheduled( 'wi_create_daily_property' );
-
-	dd($timestamp);
-
-	//If $timestamp == false schedule daily backups since it hasn't been done previously
-	if( $timestamp == false ){
-		//Schedule the event for right now, then to repeat daily using the hook 'wi_create_daily_backup'
-		wp_schedule_event( time(), 'daily', 'wi_create_daily_property' );
-	}
-}
-
-//Hook our function , wi_create_backup(), into the action wi_create_daily_backup
-add_action( 'wi_create_daily_property', 'wi_create_property' );
-
-function wi_create_property(){
-	add_action('init', 'wptrebNightly');
-}*/
